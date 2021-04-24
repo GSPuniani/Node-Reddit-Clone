3d8e0945802c687289cea6ccad8d05f911c6d5b5
Gobind Puniani <gobindpuniani@gmail.com>
2021-04-23 21:03:56 -0700

    Users can see subreddits

--- controllers/posts.js
+++ controllers/posts.js
@@ -36,4 +36,15 @@ module.exports = app => {
           });
     });
 
+    // SUBREDDIT
+    app.get("/n/:subreddit", function(req, res) {
+      Post.find({ subreddit: req.params.subreddit }).lean()
+      .then(posts => {
+        res.render("posts-index", { posts });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    });
+
 };
