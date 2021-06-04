edad1ffafa51b330dc2d94d8eb748aee8f49cebe
Gobind Puniani <gobindpuniani@gmail.com>
2021-06-03 23:44:12 -0700

    Added password support links to login page

--- models/user.js
+++ models/user.js
@@ -3,13 +3,14 @@ const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  createdAt: { type: Date },
-  updatedAt: { type: Date },
-  password: { type: String, select: false },
-  username: { type: String, required: true }
-},
-  {timestamps: {createdAt: 'created_at'}}
-);
+    createdAt: { type: Date },
+    updatedAt: { type: Date },
+    password: { type: String, select: false },
+    username: { type: String, required: true },
+    posts : [{ type: Schema.Types.ObjectId, ref: "Post" }],
+  },
+    { timestamps: { createdAt: 'created_at' } }
+  );
 
 // Must use function here! ES6 => functions do not bind this!
 UserSchema.pre("save", function (next) {
